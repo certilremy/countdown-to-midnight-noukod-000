c6c4c5486f70fd2ac052fd16370050e5a3927cdf
Certil Remy <certil@noukod.com>
2018-09-19 18:31:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 10
+  10.times do
+  puts "#{x} SECOND(S)!"
+    sleep(1)
+  x-=1
+
+  end
+
+  if x = 1
+    message ="HAPPY NEW YEAR!"
+   puts "HAPPY NEW YEAR!"
+   return message
+  end
+end
+
+def countdown_with_sleep (countdown)
+  sleep(5)
 end
